e2ecaba9626f2f9bc40234f7c6b34945a31d5910
weilai <1083344129@qq.com>
2025-08-11 17:07:03 +0800

    [feat] 优化博客详情

--- src/api/common.ts
+++ src/api/common.ts
@@ -7,9 +7,11 @@ const BASE_URL = '/common'
 
 
 export async  function getImgName() {
+    console.log("进行加密咯")
     const path = ""
     const timestamp = Math.floor(Date.now() / 1000);
     const secret = import.meta.env.VITE_WEB_SECRET
     const sign = await generateSign(path, timestamp, secret);
+    console.log("加密结果")
     return request.get(`${BASE_URL}/random?path=${path}&t=${timestamp}&sign=${sign}`)
 }
